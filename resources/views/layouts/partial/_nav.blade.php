a08ac74abfa771fb1212c1f20d1f8226e52a6304
John Chishugi <jkchishugi@gmail.com>
2017-11-24 22:47:02 +0200

    add set active route helperPage

--- resources/views/layouts/partial/_nav.blade.php
+++ resources/views/layouts/partial/_nav.blade.php
@@ -12,8 +12,8 @@
   </div>
   <div id="navbar" class="navbar-collapse collapse">
     <ul class="nav navbar-nav">
-      <li ><a href="{{ route('home_path') }}">Home</a></li>
-      <li><a href="{{ route('about_path') }}">About</a></li>
+      <li class="{{ set_active_route('home_path') }}"><a href="{{ route('home_path') }}">Home</a></li>
+      <li class="{{ set_active_route('about_path') }}"><a href="{{ route('about_path') }}">About</a></li>
       <li><a href="#contact">Artisans</a></li>
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Planet <span class="caret"></span></a>
